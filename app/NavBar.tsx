19b3888ef232ab9ced55c2543e2a84c5cf6f27f6
James Vardy <james.vardyc@gmail.com>
2023-06-17 21:07:52 +0100

    remove redundant code, style  NavBar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,52 +1,24 @@
-"use client";
-
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Link from "next/link";
-import { useState } from "react";
 
 import { faEnvelope } from "@fortawesome/free-regular-svg-icons";
 import { faInstagram } from "@fortawesome/free-brands-svg-icons";
 
-async function getProfessionalDetails() {
-  const res = await fetch(
-    "https://edit.marvinmarvinproductions.com/api/professional-detail?populate=*"
-  );
-
-  if (!res.ok) {
-    // This will activate the closest `error.js` Error Boundary
-    throw new Error("Failed to fetch data");
-  }
-
-  return res.json();
-}
-
 export default function NavBar() {
-  const [click, setClick] = useState(false);
-  const changeClick = () => setClick(!click);
-
-  // const [instagramLink, setInstagramLink] = useState("blank");
-  // const professionalDetails: professionalDetails =
-  //   await getProfessionalDetails();
-  // setInstagramLink(professionalDetails.data.attributes.instagramLink);
-
   return (
-    <nav>
-      <div className="flex justify-between py-4">
-        <div className="justify-between items-center mx-4 my-4 flex md:justify-center ">
-          <div className="">
-            <h1 className="text-2xl md:text-4xl text-left">
-              <Link href={"/"} className="capitalize">
-                MarvinMarvin Productions
-              </Link>
-            </h1>
-          </div>
+    <nav className="">
+      <div className="flex py-4 flex-col justify-center items-center md:flex-row md:py-8">
+        <div className="justify-between items-center flex py-4">
+          <h1 className="text-2xl text-center md:text-4xl md:text-left">
+            <Link href={"/"}>MarvinMarvin Productions</Link>
+          </h1>
         </div>
 
-        <div className="flex items-center mx-4 my-4">
+        <div className="flex items-center py-4">
           <div className="flex text-xl lg:mx-24">
-            {/* <Link href={"/works"} className="px-2 md:px-5 hidden md:block">
+            <Link href={"/works"} className="px-2 md:px-5">
               Works
-            </Link> */}
+            </Link>
             <Link
               href={"mailto:marvinmarvinproductions@gmail.com"}
               className="px-5"
