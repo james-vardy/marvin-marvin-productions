a23174c322f71e25702b9edf5cd5a79b345c759e
james <james.vardyc@gmail.com>
2025-08-30 19:36:31 +0100

    remove duplicate

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,6 @@ import { SpeedInsights } from "@vercel/speed-insights/next";
 const font = Inter({ subsets: ["latin"] });
 
 import "@fortawesome/fontawesome-svg-core/styles.css";
-import { SpeedInsights } from "@vercel/speed-insights/next";
 import { config } from "@fortawesome/fontawesome-svg-core";
 import NavBar from "@/components/NavBar";
 config.autoAddCss = false;
@@ -23,7 +22,6 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`bg-gray-50 ${font.className}`}>
-        <SpeedInsights />
         <div className="min-h-screen flex flex-col">
           <NavBar />
           <div className="flex-1">{children}</div>
